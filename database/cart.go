b7512d8b4c816517d4a85c9470a3512a30a55749
cupcake08 <bhankhariaa@gmail.com>
2022-03-27 11:40:42 +0530

    implement database_setup.go file

--- database/cart.go
+++ database/cart.go
@@ -1,1 +1,31 @@
 package database
+
+import "errors"
+
+//Error Variables
+
+var (
+	ErrorNotFound         = errors.New("product not found")
+	ErrCantDecodeProduct  = errors.New("can't decode product")
+	ErrUserIdNotValid     = errors.New("user Id is not valid")
+	ErrCantUpdateUser     = errors.New("can't update user")
+	ErrCantRemoveItemCart = errors.New("can't remove item from cart")
+	ErrCantBuyCartItem    = errors.New("can't buy cart item")
+	ErrCantGetItem        = errors.New("can't get item")
+)
+
+func AddProductToCart() {
+
+}
+
+func RemoveItemFromCart() {
+
+}
+
+func BuyFromCart() {
+
+}
+
+func InstantBuyer() {
+
+}
--- database/database_setup.go
+++ database/database_setup.go
@@ -1,1 +1,46 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBSet() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://titsy:ankit123@cluster0.ysk2v.mongodb.net/ecommerce?retryWrites=true&w=majority"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	if err := client.Connect(ctx); err != nil {
+		log.Fatal(err)
+	}
+
+	if err := client.Ping(context.TODO(), nil); err != nil {
+		log.Println("failed to connect to mongodb :(")
+		return nil
+	}
+
+	fmt.Println("Connected to mongodb :)")
+	return client
+}
+
+var Client *mongo.Client = DBSet()
+
+func UserData(client *mongo.Client, collectionName string) *mongo.Collection {
+	var collection *mongo.Collection = client.Database("Ecommerce").Collection(collectionName)
+	return collection
+}
+
+func ProductData(client *mongo.Client, collectionName string) *mongo.Collection {
+	var productCollection *mongo.Collection = client.Database("Ecommerce").Collection(collectionName)
+	return productCollection
+}
