23454adf25a774bd0c32ef17fe1bcb9bee3c4f12
cupcake08 <bhankhariaa@gmail.com>
2022-04-14 14:44:58 +0530

    cart functions work

--- database/cart.go
+++ database/cart.go
@@ -1,6 +1,15 @@
 package database
 
-import "errors"
+import (
+	"context"
+	"errors"
+	"log"
+
+	"github.com/cupcake08/ecommerce-go/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 //Error Variables
 
@@ -14,12 +23,58 @@ var (
 	ErrCantGetItem        = errors.New("can't get item")
 )
 
-func AddProductToCart() {
+func AddProductToCart(ctx context.Context, prodCollection, userCollection *mongo.Collection, productID primitive.ObjectID, userID string) error {
+	searchFromDB, err := prodCollection.Find(ctx, bson.M{"_id": productID})
 
+	if err != nil {
+		log.Println(err)
+		return ErrorNotFound
+	}
+
+	var productCart []models.ProductUser
+
+	err = searchFromDB.All(ctx, &productCart)
+
+	if err != nil {
+		log.Println(err)
+		return ErrCantDecodeProduct
+	}
+
+	id, err := primitive.ObjectIDFromHex(userID)
+
+	if err != nil {
+		log.Println(err)
+		return ErrUserIdNotValid
+	}
+
+	filter := bson.D{primitive.E{Key: "_id", Value: id}}
+
+	update := bson.D{{Key: "$push", Value: bson.E{Key: "usercart", Value: bson.D{{Key: "$each", Value: productCart}}}}}
+
+	_, err = userCollection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		log.Fatal(err)
+		return ErrCantUpdateUser
+	}
+	return nil
 }
 
-func RemoveItemFromCart() {
+func RemoveItemFromCart(ctx context.Context, userCollection, prodCollection *mongo.Collection, productID primitive.ObjectID, userID string) error {
+	id, err := primitive.ObjectIDFromHex(userID)
+	if err != nil {
+		log.Println(err)
+		return ErrUserIdNotValid
+	}
 
+	filter := bson.D{{Key: "_id", Value: id}}
+
+	update := bson.M{"$pull": bson.M{"usercart": bson.M{"_id": productID}}}
+	_, err = userCollection.UpdateMany(ctx, filter, update)
+	if err != nil {
+		log.Println(err)
+		return ErrCantRemoveItemCart
+	}
+	return nil
 }
 
 func BuyItemFromCart() {
